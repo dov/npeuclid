a0d0c5b05daac090732dbf9dfeb591f1d0c707df
Dov Grobgeld <dov.grobgeld@gmail.com>
2020-05-01 13:08:03 +0300

    Project management fixed long description.

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 import setuptools
 from distutils.core import setup
 
 setup(name='npeuclid',
       py_modules=['npeuclid'],
-      version='0.1',
+      version='0.1.1',
       license='lgpl-2.1',
       description='Fast 2D and 3D vector geometry module',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='Dov Grobgeld',
       author_email='dov.grobgeld@gmail.com',
       url='http://github.com/dov/npeuclid',
-      download_url = 'https://github.com/dov/npeuclid/archive/v0.1.tar.gz',
+      download_url = 'https://github.com/dov/npeuclid/archive/v0.1.1.tar.gz',
       keywords = ['math','geometry'],
       install_requires=[            
           'numpy',
